5a573eaa6f8728e987fbf0b310d4149a0f923499
shakil-121 <shahadatshakil76@gmail.com>
2023-06-27 17:54:45 +0600

    folder stucture done for move final project

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,17 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import Root from "../Layout/Root/Root";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element:<Root></Root>,
+      children:[
+        {
+            
+        }
+      ]
     },
   ]); 
 
