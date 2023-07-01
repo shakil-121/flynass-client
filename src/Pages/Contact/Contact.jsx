543d2807acd118085df4ea3e2e640b9561c11519
shakil-121 <shahadatshakil76@gmail.com>
2023-07-01 18:41:02 +0600

    contact page done

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,8 @@
 import "./Contact.css";
 import React, { useRef } from "react";
-import emailjs from "@emailjs/browser"; 
+import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from "react-toastify";
-
+import { FaMapMarkerAlt, FaMobileAlt, FaEnvelope } from "react-icons/fa";
 
 const Contact = () => {
   const form = useRef();
@@ -19,8 +19,8 @@ const Contact = () => {
       )
       .then(
         (result) => {
-          console.log(result.text); 
-          toast("Message Send Successfully !")
+          console.log(result.text);
+          toast("Message Send Successfully !");
         },
         (error) => {
           console.log(error.text);
@@ -39,7 +39,7 @@ const Contact = () => {
           Send Us a Message
         </h1>
       </div>
-      <div className="bg-slate-300  m-14 md:flex gap-4 rounded-xl">
+      <div className="bg-slate-200  m-14 md:flex gap-4 rounded-xl">
         <div className="md:w-2/3 p-10">
           <form ref={form} onSubmit={sendEmail}>
             <div className="form-control w-full">
@@ -74,15 +74,37 @@ const Contact = () => {
                 className="textarea textarea-bordered h-24"
                 placeholder="Bio"
               ></textarea>
-            </div> 
+            </div>
             <br />
-            <input className="btn hover:bg-orange-600 font-bold hover:text-white" type="submit" value="Send Message" />
+            <input
+              className="btn hover:bg-orange-600 font-bold hover:text-white"
+              type="submit"
+              value="Send Message"
+            />
           </form>
         </div>
-        <div className="md:w-1/3 contact-bg bg-blue-950 p-10">
-           <h1 className="border-l-8 border-orange-400 p-2 text-2xl font-semibold text-white font-pppins">Let's get in touch</h1>
+        <div className="md:w-1/3 contact-bg bg-blue-950 p-10 text-white">
+          <h1 className="border-l-8 border-orange-400 p-2 text-2xl font-semibold text-white font-pppins">
+            Let's get in touch
+          </h1>
+          <div className="flex">
+            <FaMapMarkerAlt className="text-4xl mt-10"></FaMapMarkerAlt>
+            <p className="mt-10 ms-4">Mirpur,Dhaka-1216</p>
+          </div>
+          <div className="flex">
+            <FaMobileAlt className="text-4xl mt-10"></FaMobileAlt>
+            <p className="mt-8 ms-4">
+              +880 1633-607006 <br /> +880 1791-750192
+            </p>
+          </div>
+          <div className="flex">
+            <FaEnvelope className="text-4xl mt-10"></FaEnvelope>
+            <p className="mt-8 ms-4">
+              hasibmeem9@gmail.com <br /> ahamedimraz699@gmail.com
+            </p>
+          </div>
         </div>
-      </div> 
+      </div>
       <ToastContainer />
     </div>
   );
