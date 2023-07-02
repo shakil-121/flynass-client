4cd6f58f4e7373c51aaaca210ab789f47a2a0246
coders rabbi <pcrabbi2020@gmail.com>
2023-07-02 18:15:16 +0600

    choose us start

--- src/Component/SectionTitle.jsx
+++ src/Component/SectionTitle.jsx
@@ -2,7 +2,7 @@ import './SectionTitile.css'
 
 const SectionTitle = ({heading, subHeading}) => {
     return (
-        <div className="mx-auto text-center md:w-4/12 my-8">
+        <div className="mx-auto text-center my-8">
             <p className="text-design text-5xl text-[#ff8a00] mb-2 font-bold">{subHeading}</p>
             <h3 className="text-4xl uppercase py-4 font-bold">{heading}</h3>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeBanner from './HomeBanner/HomeBanner';
 import WhatWeDo from './WhatWeDo/WhatWeDo';
+import ChooseUs from './ChooseUs/ChooseUs';
 
 const Home = () => {
     return (
         <div>
             <HomeBanner/>
             <WhatWeDo/>
+            <ChooseUs/>
         </div>
     );
 };
