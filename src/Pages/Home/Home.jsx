596032e81abc253870fdd6a2e523fe4e8c2f9611
shakil-121 <shahadatshakil76@gmail.com>
2023-07-03 21:08:52 +0600

    just for test commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,8 @@ import HeroSection from './HeroSection';
 const Home = () => {
     return (
         <div>
-           <HeroSection></HeroSection>
+           <HeroSection></HeroSection> 
+           
         </div>
     );
 };
