d2f49c3e8c47098a66412e5ca421dbddace53caf
shakil-121 <shahadatshakil76@gmail.com>
2023-07-04 22:29:18 +0600

    Hero section Done

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,8 +3,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper"; 
-import "./HeroSection.scss"
+import { Autoplay, Pagination, Navigation } from "swiper";
+import "./HeroSection.scss";
+import van from "../../assets/images/van.png";
+import Lottie from "lottie-react";
+import bikeanimation from "../../../public/Delivery boy on the way.json";
+import delivaryanimation from "../../../public/2 animation combined.json";
 
 import { FaChevronCircleRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -20,11 +24,10 @@ const HeroSection = () => {
   return (
     <div className="mt-14">
       <Swiper
-
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 3500,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -36,17 +39,69 @@ const HeroSection = () => {
         className="mySwiper  -z-10"
       >
         <SwiperSlide>
-          <div className="h-96 bg-orange-600">
+          <div className="h-[75vh] bg-[#1e8fd0] flex"> 
+          <div className="md:w-[80%] px-10 py-4">
+          <h1 className="text-design text-9xl text-white font-bold">
+                {" "}
+                সম্পূর্ণ
+              </h1>
+              <h1 className="text-9xl text-white font-bold">বাংলাদেশ</h1>
+             <br />  
+             <br />
+             <p className="text-white text-2xl">Lorem ipsum dolor sit amet  Sunt enim maiores numquam ducimus <br /> necessitatibus vero qui quos quibusdam labore non!</p> <br />
+             <button className="btn btn-outline rounded-none px-6 border-white text-white text-xl">VIEW SERVICES</button>
 
-            <div className="background-animated"></div>
+          </div>
+            <div className="background-animated w-[80%] md:w-[24%]  md:right-0">
+              <img className="px-6 pt-16" src={van} alt="" />
+            </div>
             </div> 
-          
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-96 bg-orange-600"></div>
+          <div className="h-[75vh] bg-[#1e8fd0] flex">
+            <div className="md:w-[60%] px-10 py-4">
+              <h1 className="text-design text-9xl text-white font-bold">
+                TO EVERY
+              </h1>
+              <h1 className="text-9xl text-white font-bold">DISTRICT</h1>
+              <br />
+              <br />
+              <p className="text-white text-2xl">
+                Lorem ipsum dolor sit amet Sunt enim maiores numquam ducimus{" "}
+                <br /> necessitatibus vero qui quos quibusdam labore non!
+              </p>{" "}
+              <br />
+              <button className="btn btn-outline rounded-none px-6 border-white text-white text-xl">
+                VIEW SERVICES
+              </button>
+            </div>
+            <div className="w-[80%] md:w-[40%]  md:right-0">
+            <Lottie className="opacity-70" animationData={bikeanimation} loop={true} />
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-96 bg-orange-600 "></div>
+          <div className="h-[75vh] bg-[#1e8fd0] flex">
+            <div className="md:w-[60%] px-10 py-4">
+              <h1 className="text-design text-9xl text-white font-bold">
+                READY
+              </h1>
+              <h1 className="text-9xl text-white font-bold">TO MOVE</h1>
+              <br />
+              <br />
+              <p className="text-white text-2xl">
+                Lorem ipsum dolor sit amet Sunt enim maiores numquam ducimus{" "}
+                <br /> necessitatibus vero qui quos quibusdam labore non!
+              </p>{" "}
+              <br />
+              <button className="btn btn-outline rounded-none px-6 border-white text-white text-xl">
+                VIEW SERVICES
+              </button>
+            </div>
+            <div className="w-[80%] md:w-[40%] pt-28  md:right-0">
+            <Lottie className="opacity-70" animationData={delivaryanimation} loop={true} />
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
