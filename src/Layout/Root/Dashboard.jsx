06d5e7973b29c36978b11652ba3998216ca9ca70
shakil-121 <shahadatshakil76@gmail.com>
2023-07-09 17:10:34 +0600

    test 02

--- src/Layout/Root/Dashboard.jsx
+++ src/Layout/Root/Dashboard.jsx
@@ -7,6 +7,7 @@ const Dashboard = () => {
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
+          
           <label
             htmlFor="my-drawer-2"
             className="btn btn-primary drawer-button lg:hidden"
