58f316edd16f24896645b561aab5d4694d8d7030
shakil-121 <shahadatshakil76@gmail.com>
2023-07-11 21:29:54 +0600

    Analatics page done for admin

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -6,6 +6,10 @@ import Services from "../Pages/Services/Services";
 import SignUp from "../Pages/LoginAndSignup/SignUp";
 import Login from "../Pages/LoginAndSignup/Login";
 import Contact from "../Pages/Contact/Contact";
+import Coverage from "../Pages/Coverage/Coverage";
+import Dashboard from "../Layout/Root/Dashboard";
+import Profile from "../Dashboard/Profile/Profile";
+import AdminAnalysis from "../Dashboard/Admin/AdminAnalysis";
 
 const router = createBrowserRouter([
   {
@@ -49,6 +53,10 @@ const router = createBrowserRouter([
       {
         path: 'profile',
         element: <Profile></Profile>,
+      },
+      {
+        path: 'admin-analysis',
+        element: <AdminAnalysis></AdminAnalysis>,
       }
     ]
   }
