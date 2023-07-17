1e8fcaf794758fbed04013a6409c406ac0600987
shakil-121 <shahadatshakil76@gmail.com>
2023-07-17 16:21:34 +0600

    axios secure , useAuth and userInfo custom hook and dashboard profile page data dybamic done

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,20 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router/Router.jsx";
 import AuthProvider from "./AuthProvider/AuthProvider";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
