626ac36d4d9350cd31db9d389ba326af78cc8695
shakil-121 <shahadatshakil76@gmail.com>
2023-07-24 15:18:04 +0600

    Download and print button done

--- src/Component/Invoice/Invoice.jsx
+++ src/Component/Invoice/Invoice.jsx
@@ -4,34 +4,60 @@ import useParcel from '../../Hooks/useParcel';
 import { FaDownload, FaPrint, FaRegEdit } from 'react-icons/fa';
 import ParcelEdit from '../ParcelEdit/ParcelEdit';
 import { useState } from 'react';
+import html2pdf from 'html2pdf.js';
 
 const Invoice = () => {
     const [showModal, setShowModal] = useState(false);
     const parcelId = useParams();
     const allParce = useParcel();
-    const invoiceParcel = allParce.find(parcel => parcel._id === parcelId.id);
-    const payableAmount = (parseFloat(invoiceParcel.total_amount) + parseFloat(invoiceParcel.product_amount));
+    const invoiceParcel = allParce.find(parcel => parcel?._id === parcelId.id);
+    const payableAmount = (parseFloat(invoiceParcel?.total_amount) + parseFloat(invoiceParcel?.product_amount));
+
+    const handleDownload = () => {
+        const element = document.getElementById('invoice-section');
+        const opt = {
+            margin:       10,
+            filename:     'invoice.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+
+        // Create a PDF using html2pdf.js and download it
+        html2pdf().from(element).set(opt).save();
+    };
+
+    const printDivContent = () => {
+        const printContents = document.getElementById('invoice-section').innerHTML;
+        const originalContents = document.body.innerHTML;
+
+        document.body.innerHTML = printContents;
+        window.print();
+
+        // Restore the original content after printing
+        document.body.innerHTML = originalContents;
+    };
 
     return (
         <div className='flex justify-center items-center h-full'>
             <div>
                 <div className='flex justify-between mb-10'>
                     <button onClick={() => setShowModal(true)} className='btn btn-primary flex items-center justify-center'><FaRegEdit />Edit</button>
-                    <button className='btn btn-primary flex items-center justify-center'><FaDownload />Download</button>
-                    <button className='btn btn-primary flex items-center justify-center'><FaPrint />Print</button>
+                    <button onClick={handleDownload} className='btn btn-primary flex items-center justify-center'><FaDownload />Download</button>
+                    <button onClick={printDivContent} className='btn btn-primary flex items-center justify-center'><FaPrint />Print</button>
                 </div>
-                <div className='bg-gray-100 rounded-xl'>
+                <div className='bg-gray-100 rounded-xl' id='invoice-section'>
                     <div className='flex items-center justify-between mb-8'>
                         <img className='w-36' src={logo} alt="" />
                         <h1 className='text-3xl font-serif font-semibold bg-gray-300 ps-8 pe-5 py-4 rounded-bl-full '>Invoice</h1>
                     </div>
                     <div className='p-5'>
-                        <h1 className='text-xl text-center font-pppins mb-10'>Tracking ID: <br /> {invoiceParcel._id}</h1>
-                        <h2 className='text-2xl font-serif mb-3'>Name: <span>{invoiceParcel.name}</span></h2>
-                        <h2 className='text-2xl font-serif mb-3'>Phone: <span>{invoiceParcel.phone}</span></h2>
-                        <h2 className='text-2xl font-serif mb-3'>Cutomer Address: <span>{invoiceParcel.to_address}</span></h2>
-                        <h2 className='text-2xl font-serif mb-3'>Delivery Charge: <span>{invoiceParcel.total_amount}Tk.</span></h2>
-                        <h2 className='text-2xl font-serif mb-3'>Parcel Amount: <span>{invoiceParcel.product_amount}Tk.</span></h2>
+                        <h1 className='text-xl text-center font-pppins mb-10'>Tracking ID: <br /> {invoiceParcel?._id}</h1>
+                        <h2 className='text-2xl font-serif mb-3'>Name: <span>{invoiceParcel?.name}</span></h2>
+                        <h2 className='text-2xl font-serif mb-3'>Phone: <span>{invoiceParcel?.phone}</span></h2>
+                        <h2 className='text-2xl font-serif mb-3'>Customer Address: <span>{invoiceParcel?.to_address}</span></h2>
+                        <h2 className='text-2xl font-serif mb-3'>Delivery Charge: <span>{invoiceParcel?.total_amount}Tk.</span></h2>
+                        <h2 className='text-2xl font-serif mb-3'>Parcel Amount: <span>{invoiceParcel?.product_amount}Tk.</span></h2>
                         <hr className='border-2' />
                         <h2 className='text-2xl font-serif mb-3'>Payable Amount: <span>{payableAmount}Tk.</span></h2>
 
@@ -46,3 +72,7 @@ const Invoice = () => {
 };
 
 export default Invoice;
+
+
+
+              
