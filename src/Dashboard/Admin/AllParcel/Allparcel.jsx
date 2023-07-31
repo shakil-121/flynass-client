a1699222c8fbde23d3f7219aa824ba6c61e96452
shakil-121 <shahadatshakil76@gmail.com>
2023-07-31 18:40:29 +0600

    admin panel all parcel order status update done

--- src/Dashboard/Admin/AllParcel/Allparcel.jsx
+++ src/Dashboard/Admin/AllParcel/Allparcel.jsx
@@ -3,15 +3,34 @@ import { useState } from 'react';
 import ParcelRejectForm from './ParcelRejectForm';
 import useParcel from '../../../Hooks/useParcel';
 import { FaEye, FaRegEdit, FaTrashAlt } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
+import Swal from "sweetalert2";
+
 
 const AllParcel = () => {
     const [showModal, setShowModal] = useState(false);
     const allParce = useParcel();
     // console.log(allParce);
 
-    const handleStatusUpdate = status => {
-        console.log(status);
+    const handleStatusUpdate = (status,id) => {
+        console.log(status,id);   
+        const statusUpdate={status}
+        fetch(`http://localhost:5000/order/status/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(statusUpdate)
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.acknowledged) {
+                    Swal.fire({
+                        title: "Update",
+                        text: "Status update successfully",
+                        icon: "success",
+                        confirmButtonText: "Update",
+                    });
+                }
+            });
     }
     return (
         <div>
@@ -49,12 +68,12 @@ const AllParcel = () => {
                                     <div className="dropdown dropdown-bottom text-black">
                                         <label tabIndex={0} className=""><button className='btn bg-[#1E62D4] hover:bg-[#1E62D4] text-white'>{parcel.status}</button></label>
                                         <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-48">
-                                            <li onClick={() => handleStatusUpdate('picked')}><a>Picked</a></li>
-                                            <li onClick={() => handleStatusUpdate('on the way')}><a>On The Way</a></li>
-                                            <li onClick={() => handleStatusUpdate('delivered')}><a>Deliverd</a></li>
-                                            <li onClick={() => handleStatusUpdate('hold')}><a>Hold</a></li>
-                                            <li onClick={() => handleStatusUpdate('returned')}><a>Return</a></li>
-                                            <li onClick={() => handleStatusUpdate('returned to merchant')}><a>Return to Mechant</a></li>
+                                            <li onClick={() => handleStatusUpdate('picked',parcel?._id)}><a>Picked</a></li>
+                                            <li onClick={() => handleStatusUpdate('on the way',parcel?._id)}><a>On The Way</a></li>
+                                            <li onClick={() => handleStatusUpdate('delivered',parcel?._id)}><a>Deliverd</a></li>
+                                            <li onClick={() => handleStatusUpdate('hold',parcel?._id)}><a>Hold</a></li>
+                                            <li onClick={() => handleStatusUpdate('returned',parcel?._id)}><a>Return</a></li>
+                                            <li onClick={() => handleStatusUpdate('returned to merchant',parcel?._id)}><a>Return to Mechant</a></li>
                                         </ul>
                                     </div>
                                 </td>
