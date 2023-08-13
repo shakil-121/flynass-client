3d08145dac7e313ae156f1d4d34441a29804a1a5
shakil-121 <shahadatshakil76@gmail.com>
2023-08-13 19:58:43 +0600

    ball change

--- src/Dashboard/Notice/Notice.jsx
+++ src/Dashboard/Notice/Notice.jsx
@@ -1,6 +1,9 @@
 // import { AiFillAlert, IconName } from "react-icons/ai";
 import { IoMdNotifications } from "react-icons/io";
-import useNotice from "../../Hooks/useNotice";
+import useNotice from "../../Hooks/useNotice";  
+import Lottie from "lottie-react";
+import ball from "../../../public/ball.json"
+
 
 const Notice = () => { 
     const noticeUpdate=useNotice()  
@@ -8,7 +11,12 @@ const Notice = () => {
 
     return (
         <div className='mt-5 px-3 bg-[#E8F6FC] p-5 flex gap-3 rounded-md'>
-            <IoMdNotifications className="text-4xl text-[#D53343]" />
+            {/* <IoMdNotifications className="text-4xl text-[#D53343]" /> */} 
+            <Lottie
+                className="opacity-70 h-16"
+                animationData={ball}
+                loop={true}
+              />
             <h4 className='font-semibold flex-1'>
                {notice}
             </h4>
